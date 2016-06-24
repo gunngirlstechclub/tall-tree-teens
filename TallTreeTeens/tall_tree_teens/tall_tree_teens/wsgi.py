5a5805cf4015d02cc38049db24f492c331ef2de3
Maggie Wang <maggi3wang@gmail.com>
2016-06-23 20:50:32 -0700

    Adding TallTreeTeens

---
+++ TallTreeTeens/tall_tree_teens/tall_tree_teens/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tall_tree_teens project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tall_tree_teens.settings")
+
+application = get_wsgi_application()
